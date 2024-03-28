89d7de7aa978017a506e4f935bf4e8f55ce2087f
João Gabriel C. da Cruz <gabriel_cassuriaga@hotmail.com>
2024-03-28 20:09:33 -0300

    Cometi um erro ao enviar

--- CosItems/src/main/java/br/com/fundatec/CosItems/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fundatec.CosItems.repository;
-
-import br.com.fundatec.CosItems.model.ProductModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<ProductModel, String> {
-}
--- CosItems/src/main/java/br/com/fundatec/CosItems/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fundatec.CosItems.repository;
-
-import br.com.fundatec.CosItems.model.UserModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-}
